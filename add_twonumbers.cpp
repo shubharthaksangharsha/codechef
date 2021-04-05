c502cad0198205c753ee8d9b88150d2387e8ac37
Shubharthak Sangharsha <shubharthaksangharsha@gmail.com>
2021-04-05 10:58:08 +0530

    Add Two Numbers problem with solution

---
+++ add_twonumbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int T,num1,num2,total;
+	cin >> total;
+	
+	
+	
+	for(int i = 0 ; i< total; i++){
+		cin>>num1>>num2;
+		T = num1+num2;
+		cout<<T<<endl;
+	}
+		
+		
+	return 0;
+	
+
+}
